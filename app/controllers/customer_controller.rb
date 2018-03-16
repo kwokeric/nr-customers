90c4fd9a9c5318a4a3142caed1e8d06da8808643
kwokeric <eric.kwok@vevo.com>
2018-03-15 21:51:23 -0700

    Implement search in controller

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,4 +1,18 @@
 class CustomerController < ApplicationController
   def index
+    Rails.logger.info params["name"]
+
+    if params.include?("q")
+      query = "#{params["q"].downcase}%"
+      result = Customer.where("first_name LIKE ? OR last_name LIKE ?", query, query)
+    else
+      result = Customer.all
+    end
+
+    render json: result
   end
+
+  # def create
+  #   Customer.new(params[:first_name], params[:last_name])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'search/index'
-
-  get 'customer/index'
-
-  get 'home/index'
-  get 'home/timestamp'
-
-  root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customer, only: [:index]
 end
